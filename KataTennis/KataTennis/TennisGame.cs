212e472efda9d4bce9399e6776109bf1678883cf
unknown <d.wagner@.pharmatechnik.de>
2011-06-10 14:39:42 +0200

    Added Kata Tennis

---
+++ KataTennis/KataTennis/TennisGame.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace KataTennis {
+    public class TennisGame {
+        private static string GetPunkte(int count) {
+            if (count == 0) {
+                return "0";
+            } else if (count == 1) {
+                return "15";
+            } else if (count == 2) {
+                return "30";
+            } else if (count == 3) {
+                return "40";
+            }
+            throw new ArgumentOutOfRangeException();
+        }
+
+        private int _spieler1;
+        private int _spieler2;
+
+        public string Score() {
+            if (IstZuEnde()) {
+                return "Sieg Spieler " + (_spieler1 > _spieler2
+                           ? "1"
+                           : "2");
+            }
+            if (_spieler1 >= 3 && _spieler2 >= 3) {
+                if (_spieler1 == _spieler2) {
+                    return "Einstand";
+                }
+                if (_spieler1 > _spieler2) {
+                    return "Vorteil Spieler 1";
+                }
+                if (_spieler1 < _spieler2) {
+                    return "Vorteil Spieler 2";
+                }
+            }
+            return GetPunkte(_spieler1) + ":" + GetPunkte(_spieler2);
+        }
+
+        public void Spieler1Punktet() {
+            _spieler1++;
+        }
+
+        public void Spieler2Punktet() {
+            _spieler2++;
+        }
+
+        public bool IstZuEnde() {
+            if (_spieler1 >= 4 && _spieler1 >= (_spieler2 + 2)) {
+                return true;
+            }
+            if (_spieler2 >= 4 && _spieler2 >= (_spieler1 + 2)) {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ KataTennis/KataTennis/Tests.cs
@@ -0,0 +1,156 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace KataTennis {
+    [TestFixture]
+    public class Tests {
+        private TennisGame _tennisGame;
+
+        [SetUp]
+        public void SetupTennisGame() {
+            _tennisGame = new TennisGame();
+        }
+
+        [Test]
+        public void Teste_Keine_Punkte_Ergibt_0_Zu_0 () {
+            Assert.That(_tennisGame.Score(), Is.EqualTo("0:0"));
+        }
+
+        [Test]
+        public void Teste_Einer_Punktet_Einmal_Ergibt_15_Zu_0() {
+            _tennisGame.Spieler1Punktet();
+            Assert.That(_tennisGame.Score(), Is.EqualTo("15:0"));
+        }
+
+        [Test]
+        public void Teste_Beide_Punkten_Einmal_Ergibt_15_Zu_15() {
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler2Punktet();
+            Assert.That(_tennisGame.Score(), Is.EqualTo("15:15"));
+        }
+
+        [Test]
+        public void Teste_Einer_Punktet_Zweimal_Ergibt_30_Zu_0() {
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler1Punktet();
+            Assert.That(_tennisGame.Score(), Is.EqualTo("30:0"));
+        }
+
+        [Test]
+        public void Teste_Einer_Punktet_Dreimal_Ergibt_40_Zu_0() {
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler1Punktet();
+            Assert.That(_tennisGame.Score(), Is.EqualTo("40:0"));
+        }
+
+        [Test]
+        public void Teste_Spiel_Nicht_Zu_Ende() {
+            Assert.That(_tennisGame.IstZuEnde(), Is.False);
+        }
+
+        [Test]
+        public void Teste_Spiel_Zu_Ende_Wenn_Einer_Viermal_Punktet() {
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler1Punktet();
+            Assert.That(_tennisGame.IstZuEnde(), Is.True);
+        }
+
+        [Test]
+        public void Teste_Spiel_Zu_Ende_Wenn_Andere_Viermal_Punktet() {
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler2Punktet();
+            Assert.That(_tennisGame.IstZuEnde(), Is.True);
+        }
+
+        [Test]
+        public void Teste_Spiel_Nicht_Zu_Ende_Wenn_Beide_Viermal_Abwechselnd_Punkten() {
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler2Punktet();
+            Assert.That(_tennisGame.IstZuEnde(), Is.False);
+        }
+
+        [Test]
+        public void Teste_Spiel_Nicht_Zu_Ende_Wenn_Einer_Viermal_Punktet_Und_Der_Andere_Dreimal() {
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler1Punktet();
+            Assert.That(_tennisGame.IstZuEnde(), Is.False);
+        }
+
+        [Test]
+        public void Teste_Ergebnis_Ist_Einstand_Wenn_Beide_Dreimal_Punkten() {
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler2Punktet();
+            Assert.That(_tennisGame.Score(), Is.EqualTo("Einstand"));
+        }
+
+        [Test]
+        public void Teste_Ergebnis_Ist_Vorteil_Spieler_1_Wenn_Beide_Dreimal_Punkten_Und_Spieler1_Punktet() {
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler1Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler2Punktet();
+            _tennisGame.Spieler1Punktet();
+            Assert.That(_tennisGame.Score(), Is.EqualTo("Vorteil Spieler 1"));
+        }
+
+        [Test]
+        public void Teste_Ergebnis_Ist_Vorteil_Spieler_2_Wenn_Beide_Dreimal_Punkten_Und_Spieler2_Punktet() {
+            Spielstand(3, 4);
+            Assert.That(_tennisGame.Score(), Is.EqualTo("Vorteil Spieler 2"));
+        }
+
+        [Test]
+        public void Teste_Ergebnis_Ist_Sieg_Spieler_1_Wenn_Beide_Dreimal_Punkten_Und_Spieler1_Zweimal_Punktet() {
+            Spielstand(5, 3);
+            Assert.That(_tennisGame.Score(), Is.EqualTo("Sieg Spieler 1"));
+        }
+
+        [Test]
+        public void Teste_Ergebnis_Ist_Sieg_Spieler_2_Wenn_Beide_Dreimal_Punkten_Und_Spieler2_Zweimal_Punktet() {
+            Spielstand(3, 5);
+            Assert.That(_tennisGame.Score(), Is.EqualTo("Sieg Spieler 2"));
+        }
+
+        private void Spielstand(int spieler1, int spieler2) {
+            PunkteSpieler1(spieler1);
+            PunkteSpieler2(spieler2);
+        }
+
+
+        private void PunkteSpieler1(int count) {
+            Enumerable.Repeat(1, count).Select(n => {
+                _tennisGame.Spieler1Punktet();
+                return n;
+            }).ToArray();
+        }
+
+        private void PunkteSpieler2(int count) {
+            Enumerable.Repeat(1, count).Select(n => {
+                _tennisGame.Spieler2Punktet();
+                return n;
+            }).ToArray();
+        }
+    }
+}
