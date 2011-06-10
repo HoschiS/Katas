f1a3f39b76fd20723fb2227d144ca4ea3a9a8057
unknown <d.wagner@.pharmatechnik.de>
2011-06-10 14:39:24 +0200

    Kata Range added

---
+++ KataRange/KataRange/Range.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace KataRange {
+    public class Range {
+        private readonly IEnumerable<int> _range;
+
+        public Range(string input) {
+            _range = ParseRange(input);
+        }
+
+        public static IEnumerable<int> ParseRange(string input) {
+            if (input.Length < 5) {
+                throw new ArgumentException("Ungültige Eingabe");
+            }
+            if (!new []{ '[', '('}.Contains(input.First())) {
+                throw new ArgumentException("Ungültige Eingabe");                
+            }
+            if (!new []{ ']', ')'}.Contains(input.Last())) {
+                throw new ArgumentException("Ungültige Eingabe");                
+            }
+            IEnumerable<int> parts;
+            try {
+                parts = ParseList(input);
+            } catch (FormatException) {
+                throw new ArgumentException("Ungültige Eingabe");
+            }
+            int firstNumber = parts.First();
+            int secondNumber = parts.Last();
+            if (input[0] == '(') {
+                firstNumber++;
+            }
+            if (input.Last() == ']') {
+                secondNumber++;
+            }
+            return Enumerable.Range(firstNumber, secondNumber - firstNumber);
+        }
+
+        private static IEnumerable<int> ParseList(string comparison) {
+            return comparison.Substring(1, comparison.Length - 2).Split(',').Select(s => int.Parse(s));
+        }
+
+        public bool Contains(string comparison) {
+            if (comparison.StartsWith("{") && comparison.EndsWith("}")) {
+                return ParseList(comparison).All(value => _range.Contains(value));
+            }
+            return false;
+        }
+
+        public bool AllPoints(string comparison) {
+            if (comparison.StartsWith("{") && comparison.EndsWith("}")) {
+                return ParseList(comparison).SequenceEqual(_range);
+            }
+            return false;
+        }
+
+        public bool ContainsRange(string comparison) {
+            return ParseRange(comparison).All(value => _range.Contains(value));
+        }
+
+        public bool OverlapsRange(string comparison) {
+            return ParseRange(comparison).Any(value => _range.Contains(value));
+        }
+
+        public bool EqualsRange(string comparison) {
+            return ParseRange(comparison).SequenceEqual(_range);
+        }
+    }
+}
---
+++ KataRange/KataRange/Tests.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace KataRange {
+    [TestFixture]
+    public class Tests {
+        #region Helpers
+
+        private static void TestCaseContains(string input, string comparison, bool expectedValue) {
+            Assert.That(new Range(input).Contains(comparison), Is.EqualTo(expectedValue));
+        }
+
+        private static void TestCaseAllPoints(string input, string comparison, bool expectedValue) {
+            Assert.That(new Range(input).AllPoints(comparison), Is.EqualTo(expectedValue));
+        }
+
+        private static void TestCaseContainsRange(string input, string comparison, bool expectedValue) {
+            Assert.That(new Range(input).ContainsRange(comparison), Is.EqualTo(expectedValue));
+        }
+
+        private static void TestCaseOverlapsRange(string input, string comparison, bool expectedValue) {
+            Assert.That(new Range(input).OverlapsRange(comparison), Is.EqualTo(expectedValue));
+        }
+
+        private static void TestCaseEqualsRange(string input, string comparison, bool expectedValue) {
+            Assert.That(new Range(input).EqualsRange(comparison), Is.EqualTo(expectedValue));
+        }
+
+        #endregion Helpers
+
+        #region Input-Validation
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        public void Teste_Falsche_Eingabe_1() {
+            new Range("");
+        }
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        public void Teste_Falsche_Eingabe_2() {
+            new Range("(");
+        }
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        public void Teste_Falsche_Eingabe_3() {
+            new Range("[");
+        }
+
+        [Test, ExpectedException(typeof(ArgumentException))]
+        public void Teste_Falsche_Eingabe_4() {
+            new Range("()");
+        }
+
+        #endregion Input-Validation
+
+        [Test]
+        public void Teste_Contains_1() {
+            TestCaseContains("[2,6)", "{2,4}", true);
+        }
+
+        [Test]
+        public void Teste_Contains_2() {
+            TestCaseContains("[2,6)", "{-1,1,6,10}", false);
+        }
+
+        [Test]
+        public void Teste_AllPoints_1() {
+            TestCaseAllPoints("[2,6)", "{2,3,4,5}", true);
+        }
+
+        [Test]
+        public void Teste_AllPoints_2() {
+            TestCaseAllPoints("[2,6)", "{2,3,4,5}", true);
+        }
+
+        [Test]
+        public void Teste_AllPoints_3() {
+            TestCaseAllPoints("[2,6]", "{2,3,4,5,6}", true);
+        }
+
+        [Test]
+        public void Teste_AllPoints_4() {
+            TestCaseAllPoints("(2,6)", "{3,4,5}", true);
+        }
+
+        [Test]
+        public void Teste_AllPoints_5() {
+            TestCaseAllPoints("(2,6]", "{3,4,5,6}", true);
+        }
+
+        [Test]
+        public void Teste_ContainsRange_1() {
+            TestCaseContainsRange("[2,5)", "[7,10)", false);
+        }
+
+        [Test]
+        public void Teste_ContainsRange_2() {
+            TestCaseContainsRange("[3,5)", "[2,10)", false);
+        }
+
+        [Test]
+        public void Teste_ContainsRange_3() {
+            TestCaseContainsRange("[2,10)", "[3,5]", true);
+        }
+
+        [Test]
+        public void Teste_ContainsRange_4() {
+            TestCaseContainsRange("[3,5]", "[3,5)", true);
+        }
+
+        [Test]
+        public void Teste_OverlapsRange_1() {
+            TestCaseOverlapsRange("[2,5)", "[7,10)", false);
+        }
+
+        [Test]
+        public void Teste_OverlapsRange_2() {
+            TestCaseOverlapsRange("[2,10)", "[3,5)", true);
+        }
+
+        [Test]
+        public void Teste_OverlapsRange_3() {
+            TestCaseOverlapsRange("[3,5)", "[3,5)", true);
+        }
+
+        [Test]
+        public void Teste_OverlapsRange_4() {
+            TestCaseOverlapsRange("[2,5)", "[3,10)", true);
+        }
+
+        [Test]
+        public void Teste_OverlapsRange_5() {
+            TestCaseOverlapsRange("[3,5)", "[2,10)", true);
+        }
+
+        [Test]
+        public void Teste_EqualsRange_1() {
+            TestCaseEqualsRange("[3,5)", "[3,5)", true);
+        }
+
+        [Test]
+        public void Teste_EqualsRange_2() {
+            TestCaseEqualsRange("[2,10)", "[3,5)", false);
+        }
+
+        [Test]
+        public void Teste_EqualsRange_3() {
+            TestCaseEqualsRange("[2,5)", "[3,10)", false);
+        }
+
+        [Test]
+        public void Teste_EqualsRange_4() {
+            TestCaseEqualsRange("[3,5)", "[2,10)", false);
+        }
+    }
+}
